3342b469eec06626cbd18b60a294d7ece309bd7b
ghoon99 <ghoon99@kakao.com>
2024-11-13 22:38:50 +0900

    dsd

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ function App() {
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch((err) => console.log(err));
-  });
+  }, []);
 
   return (
     <h1>
       파이썬 서버로 부터 가져온 데이터 ::
       <br />
-      {data ? data.msg : "데이터가 없습니다."}
+      {data ? data.msg : "데이터 못가져왓어서 없습니다."}
     </h1>
   );
 }
